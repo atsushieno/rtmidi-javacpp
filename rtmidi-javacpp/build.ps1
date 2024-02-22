7b75ee1a4abf4655035ea478bea63b7a454e4ad2
Atsushi Eno <atsushieno@gmail.com>
2024-02-22 18:01:22 +0900

    make windows rtmidi.lib build consistent for Release with others
    It still fails to build locally due to `jnirtmidi.obj : error LNK2001: unresolved external symbol _rtmidi_in_create_default` etc. but might be different on CI...

--- rtmidi-javacpp/build.ps1
+++ rtmidi-javacpp/build.ps1
@@ -1,6 +1,7 @@
 
-New-Item -ItemType Directory -Force -Path "build\$Env:PLATFORM"
-Set-Location "build\$Env:PLATFORM"
+$PLATFORM = $Env:PLATFORM
+New-Item -ItemType Directory -Force -Path "build\$PLATFORM"
+Set-Location "build\$PLATFORM"
 
 $DIST = (Get-Location).Path
 
@@ -17,12 +18,12 @@ if (-not (Test-Path .patch.stamp)) {
     patch -i $ScriptDir\..\workaround-javacpp-code-generator-issue.patch -p1
     New-Item -ItemType File -Name .patch.stamp
 }
-cmake -B "build-$PLATFORM" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=false -G Ninja
-cmake --build "build-$PLATFORM"
+cmake -B "build-$PLATFORM"
+cmake --build "build-$PLATFORM" --config Release
 
-#cmake --install "build-$PLATFORM" --prefix $DIST
-New-Item -ItemType Directory -Path "$DIST\include\rtmidi" -Force
-New-Item -ItemType Directory -Path "$DIST\lib" -Force
-Copy "*.h" "$DIST\include\rtmidi"
-Copy "build-$PLATFORM\Debug\rtmidi.*" "$DIST\lib"
+cmake --install "build-$PLATFORM" --prefix $DIST
+#New-Item -ItemType Directory -Path "$DIST\include\rtmidi" -Force
+#New-Item -ItemType Directory -Path "$DIST\lib" -Force
+#Copy "*.h" "$DIST\include\rtmidi"
+#Copy "build-$PLATFORM\Debug\*" "$DIST\lib"
 
